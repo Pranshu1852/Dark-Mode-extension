16c7add7e15c6e5f97da2def61ce3067e819b96e
Pranshu Patel <pranshu.patel@simformsolutions.com>
2025-06-13 16:11:48 +0530

    Initialize: create initial structure of UI in react

---
+++ src/App.tsx
@@ -0,0 +1,159 @@
+import { Switch } from "@/components/ui/switch";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "./components/ui/tabs";
+import {
+  Command,
+  CommandEmpty,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "./components/ui/command";
+import { Slider } from "@/components/ui/slider";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import { Minus } from "lucide-react";
+import { useState } from "react";
+
+function App() {
+  const [slider, setSlider] = useState(50);
+
+  return (
+    <div className="w-full containerbox flex flex-col items-center gap-5 p-10">
+      <Switch className="bg-blue-400" />
+      <Tabs defaultValue="tab1">
+        <TabsList className="w-96">
+          <TabsTrigger className="cursor-pointer" value="tab1">
+            Tab1
+          </TabsTrigger>
+          <TabsTrigger className="cursor-pointer" value="tab2">
+            Tab2
+          </TabsTrigger>
+          <TabsTrigger className="cursor-pointer" value="tab3">
+            Tab3
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="tab1" className="flex flex-col gap-5 p-5">
+          <div className="flex flex-row gap-4">
+            <Button
+              onClick={() => {
+                setSlider((prevVal) => prevVal!==0 ? prevVal - 10: prevVal)
+              }}
+              variant="secondary"
+              size="icon"
+              className="size-8"
+            >
+              <Minus />
+            </Button>
+            <Slider
+              defaultValue={[slider]}
+              value={[slider]}
+              max={100}
+              step={1}
+            />
+            <Button
+              onClick={() => setSlider((prevVal) => prevVal!==100 ? prevVal + 10: prevVal)}
+              variant="secondary"
+              size="icon"
+              className="size-8"
+            >
+              <Plus />
+            </Button>
+          </div>
+          <div className="flex flex-row gap-4">
+            <Button
+              onClick={() => {
+                setSlider((prevVal) => prevVal!==0 ? prevVal - 10: prevVal)
+              }}
+              variant="secondary"
+              size="icon"
+              className="size-8"
+            >
+              <Minus />
+            </Button>
+            <Slider
+              defaultValue={[slider]}
+              value={[slider]}
+              max={100}
+              step={1}
+            />
+            <Button
+              onClick={() => setSlider((prevVal) => prevVal!==100 ? prevVal + 10: prevVal)}
+              variant="secondary"
+              size="icon"
+              className="size-8"
+            >
+              <Plus />
+            </Button>
+          </div>
+          <div className="flex flex-row gap-4">
+            <Button
+              onClick={() => {
+                setSlider((prevVal) => prevVal!==0 ? prevVal - 10: prevVal)
+              }}
+              variant="secondary"
+              size="icon"
+              className="size-8"
+            >
+              <Minus />
+            </Button>
+            <Slider
+              defaultValue={[slider]}
+              value={[slider]}
+              max={100}
+              step={1}
+            />
+            <Button
+              onClick={() => setSlider((prevVal) => prevVal!==100 ? prevVal + 10: prevVal)}
+              variant="secondary"
+              size="icon"
+              className="size-8"
+            >
+              <Plus />
+            </Button>
+          </div>
+        </TabsContent>
+        <TabsContent value="tab2">
+          <Command>
+            <CommandInput placeholder="Type a command or search..." />
+            <CommandList>
+              <CommandEmpty>No results found.</CommandEmpty>
+              <CommandItem>Calendar</CommandItem>
+              <CommandItem>Search Emoji</CommandItem>
+              <CommandItem>Calculator</CommandItem>
+              <CommandItem>Profile</CommandItem>
+              <CommandItem>Billing</CommandItem>
+              <CommandItem>Settings</CommandItem>
+              <CommandItem>Calendar</CommandItem>
+              <CommandItem>Search Emoji</CommandItem>
+              <CommandItem>Calculator</CommandItem>
+              <CommandItem>Profile</CommandItem>
+              <CommandItem>Billing</CommandItem>
+              <CommandItem>Settings</CommandItem>
+              <CommandItem>Calendar</CommandItem>
+              <CommandItem>Search Emoji</CommandItem>
+              <CommandItem>Calculator</CommandItem>
+              <CommandItem>Profile</CommandItem>
+              <CommandItem>Billing</CommandItem>
+              <CommandItem>Settings</CommandItem>
+              <CommandItem>Calendar</CommandItem>
+              <CommandItem>Search Emoji</CommandItem>
+              <CommandItem>Calculator</CommandItem>
+              <CommandItem>Profile</CommandItem>
+              <CommandItem>Billing</CommandItem>
+              <CommandItem>Settings</CommandItem>
+              <CommandItem>Calendar</CommandItem>
+              <CommandItem>Search Emoji</CommandItem>
+              <CommandItem>Calculator</CommandItem>
+              <CommandItem>Profile</CommandItem>
+              <CommandItem>Billing</CommandItem>
+              <CommandItem>Settings</CommandItem>
+            </CommandList>
+          </Command>
+        </TabsContent>
+        <TabsContent value="tab3">Tab3</TabsContent>
+      </Tabs>
+      <div></div>
+    </div>
+  );
+}
+
+export default App;
